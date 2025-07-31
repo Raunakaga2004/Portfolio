e10f9c04de43ca8b23a50e4e6085528d24e2977d
Raunakaga2004 <raunakaga12@gmail.com>
2025-07-31 15:03:53 +0530

    added logo animation and page transitions and snap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,15 +15,122 @@ import { useRef } from "react";
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 export default function Home() {
+  const scrollPageRef = useRef<HTMLDivElement>(null);
+
   const containerRef = useRef(null); // Ref for the component's container
   const timelineRef = useRef<any>(null); // Ref to store the timeline instance
-  const splitRef =  useRef<any>(null);
 
+  // scoll animation and snap
+  useGSAP(()=>{
+    gsap.registerPlugin(ScrollTrigger);
+
+    const containerElement = scrollPageRef.current;
+
+    const logo = scrollPageRef.current?.querySelector('#logo_name');
+
+    if(logo) {
+      gsap.set(logo, {
+        position : 'fixed'
+      })
+    }
+
+    if(!containerElement){
+      console.warn('Scroll Page ref is not available!');
+    }
+
+    const sections : HTMLElement[] = gsap.utils.toArray('.page-section', containerElement);
+
+    sections.forEach((section, i) =>{
+      if(!(section instanceof HTMLElement)){
+        console.warn("section is not a HTML element!");
+      }
+
+      ScrollTrigger.create({
+        trigger : section,
+        markers : true,
+        start : 'top top',
+        scroller : containerElement,
+        snap : {
+          
+          // delay : 0.5,
+          snapTo : 1,
+          duration : {
+            min : 0.4,
+            max : 0.6,
+          },
+          directional : true,
+          ease : 'power1.inOut',
+        }
+      })
+
+    let mm = gsap.matchMedia();
+    
+    // laptop animation 1024px 
+    mm.add({
+      isLaptop: `(min-width: 1024px)`,
+      isSmallMobile: `(min-width: 320px)`,
+      // reduceMotion: "(prefers-reduced-motion: reduce)",
+    },(context : any)=>{
+      let { isLaptop, isSmallMobile} = context.conditions;
+      
+      if(logo && i == 0){
+        // set the timeline value of logo first
+        gsap.to(logo,{
+          scale : isLaptop ? 0.3 : 0.5,
+          top : isLaptop ? -300  : -60,
+          left : isLaptop ? -300 : -100,
+          zIndex : 10,
+          scrollTrigger : {
+            trigger : section,
+            scroller : containerElement,
+            start : 'top top',
+            scrub : true,
+            snap : {
+              snapTo : 1,
+              duration : {
+                min : 0.4,
+                max : 0.6,
+              },
+              directional : true,
+              ease : 'power1.inOut',
+            }
+          },
+        })
+        gsap.to(".intro-page-content", {
+          filter : 'blur(10px)',
+          scrollTrigger : {
+            trigger : section,
+            scroller : containerElement,
+            start : 'top top',
+            scrub : true,
+            snap : {
+              snapTo : 1,
+              duration : {
+                min : 0.4,
+                max : 0.6,
+              },
+              directional : true,
+              ease : 'power1.inOut',
+            }
+          },
+        })
+      }
+    })
+
+      
+    })
+
+  }, {scope : scrollPageRef})
+
+  // intro animation
   useGSAP(()=>{
     timelineRef.current = gsap.timeline({
       defaults : {
         delay : 0,
         repeat : 0
+      },
+      onComplete : ()=>{
+        scrollPageRef.current?.classList.remove('overflow-y-hidden');
       }
     })
 
@@ -64,7 +171,7 @@ export default function Home() {
 
       timelineRef.current.to('#logo_name', {
         scale : 1,
-        y : isLaptop ? "-20px" : "-28vh",
+        y : isLaptop ? "-20px" : "-35vh",
         x : isLaptop && "-250px",
         duration : 0.5,
         delay : 0.1,
@@ -109,6 +216,10 @@ export default function Home() {
       }, 2)
     })
 
+    // timelineRef.current.set('#logo_name', {
+    //   position : 'fixed',
+    // })
+
     //tablet animation 768px
 
     //xs mobile animation 320px
@@ -122,33 +233,26 @@ export default function Home() {
   })
 
   return (
-    <div className="relative h-screen w-screen overflow-x-hidden overflow-y-scroll snap-y snap-mandatory">
-      {/* <div className="absolute z-1 h-screen w-screen flex justify-center items-center">
-        <div className="h-full w-0 border border-red-500"></div>
-
-        <div className="absolute left-[15%] h-full w-0 border border-red-500"></div>
-
-        <div className="absolute right-[15%] h-full w-0 border border-red-500"></div>
-      </div> */}
+    <div className="relative h-screen w-screen overflow-x-hidden overflow-y-hidden" ref={scrollPageRef}>
       {/* intro section */}
-      <div className="h-screen snap-start" ref={containerRef}>
+      <div className="h-screen page-section" ref={containerRef}>
 
-        <div className="absolute top-0 left-0 h-screen w-screen flex justify-center items-end overflow-y-hidden overflow-x-hidden">
+        <div className="absolute top-0 left-0 h-screen w-screen flex justify-center items-end overflow-y-hidden overflow-x-hidden intro-page">
           
           {/*w-[480px] h-[650px]*/}
-          <img id="intro_image" src={"/image/image.png"} className="max-h-[400px] lg:max-h-[700px] absolute z-2 opacity-1 overflow-x-hidden"/>
+          <img id="intro_image" src={"/image/image.png"} className="max-h-[400px] lg:max-h-[700px] absolute z-2 opacity-1 overflow-x-hidden intro-page-content"/>
           
-          <div id="intro_oval_shape" className="absolute lg:w-[530px] lg:h-[700px] w-[255px] h-[390px] bg-primary rounded-[60%/60%_60%_60%_60%] rotate-325 z-0 opacity-1 translate-y-[20px] overflow-x-hidden"/>
+          <div id="intro_oval_shape" className="absolute lg:w-[530px] lg:h-[700px] w-[255px] h-[390px] bg-primary rounded-[60%/60%_60%_60%_60%] rotate-325 z-0 opacity-1 translate-y-[20px] overflow-x-hidden intro-page-content"/>
 
           {/* Name text */}
-          <div id="logo_name" className={`${qwigley.className} absolute flex flex-col h-screen w-screen justify-center items-center z-1 -translate-y-[500px] opacity-0`}>
+          <div id="logo_name" className={`${qwigley.className} absolute flex flex-col h-screen w-screen justify-center items-center z-1 -translate-y-[500px] opacity-0 pointer-events-none`}>
             <div id="logo_raunak_name" className="text-fortext lg:text-[300px] xs:text-[96px] lg:-translate-y-[110px] rotate-[350.6deg]">Raunak</div>
             <div id="logo_agarwal_name" className="text-secondary lg:text-[250px] xs:text-[80px] lg:-translate-y-[360px] xs:-translate-y-[80px] rotate-[349.2deg]">Agarwal</div>
           </div>
         </div>
 
         {/* Intro text */}
-        <div id="intro_section" className={`${poppins.className} h-screen w-screen absolute top-0 left-0 text-fortext flex flex-col gap-[30px] justify-center items-center z-4 text-center text-wrap lg:translate-x-[210px] lg:translate-y-[90px] xs:-translate-y-[8vh] opacity-0 overflow-x-hidden`}>
+        <div id="intro_section" className={`${poppins.className} h-screen w-screen absolute top-0 left-0 text-fortext flex flex-col gap-[30px] justify-center items-center z-4 text-center text-wrap lg:translate-x-[210px] lg:translate-y-[90px] xs:-translate-y-[8vh] opacity-0 overflow-x-hidden intro-page-content`}>
           <p id="intro_text" className="lg:max-w-[700px] sm:max-w-[350px] xs:max-w-[300px] lg:text-[32px] sm:text-[20px] xs:text-[16px]">
             I'm a <span id="last_animation_highlight" className="">full-stack developer</span> specializing in building modern, scalable web applications.
           </p>
@@ -163,26 +267,33 @@ export default function Home() {
         
       </div>
       
-      <div className="snap-center h-screen text-white mt-4">
+      <div className="page-section h-screen text-white mt-4">
+      <div className="absolute z-1 h-screen w-screen flex justify-center items-center">
+        <div className="h-full w-0 border border-red-500"></div>
+
+        <div className="absolute left-[15%] h-full w-0 border border-red-500"></div>
+
+        <div className="absolute right-[15%] h-full w-0 border border-red-500"></div>
+      </div>
         offer section
       </div>
 
-      <div className="snap-start h-screen text-white mt-4">
+      <div className="page-section h-screen text-white mt-4">
         skills section
         {/* also show leetcode profile in a window maybe */}
       </div>
 
-      <div className="snap-start h-screen text-white mt-4">
+      <div className="page-section h-screen text-white mt-4">
         projects section
 
         {/* add view more button to load more */}
       </div>
 
-      <div className="snap-start h-screen text-white mt-4">
+      <div className="page-section h-screen text-white mt-4">
         contact section
       </div>
 
-      <div className="snap-start h-screen text-white mt-4">
+      <div className="page-section h-screen text-white mt-4">
         footer section
       </div>
     </div>
